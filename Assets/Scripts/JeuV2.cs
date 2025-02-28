32cb9abf81bb651bd6149f59b1af1201f92ba4a2
Victor4387 <victordinezza@videotron.ca>
2025-02-28 11:37:05 -0500

    finis exercice 2 faire exercice 1

--- Assets/Scripts/JeuV2.cs
+++ Assets/Scripts/JeuV2.cs
@@ -8,17 +8,24 @@ public class JeuV2 : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _etiquettePoints;
 
     public int pointsJeu = 0;
+    private int pointsParSeconde = 0;
 
     void Start()
     {
         pointsJeu = 0;
+        InvokeRepeating("QuantiteAugmente", 4f, 5f);
     }
 
     public void AugmenterPoints()
     {
         // Augmenter les points du jeu
-        pointsJeu++;
+        pointsJeu+= 1+pointsParSeconde;
         // Actualiser l'UI
         _etiquettePoints.text = "$" + pointsJeu.ToString();
     }
+    void QuantiteAugmente()
+    {
+        pointsParSeconde +=1;
+    }
 }
+    
